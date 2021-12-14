80c6b871427ec01c0c15526dcdd65d1260c788c4
Alex C-G <alexcg@outlook.com>
2021-12-14 11:21:49 +0100

    refactor: helper fns in one file

--- helper.py
+++ helper.py
@@ -1,12 +1,13 @@
 import glob
 import os
-from jina import Document
 import matplotlib.pyplot as plt
 from jina.types.request import Request
-from config import MAX_DOCS
+from config import MAX_DOCS, SERVER, PORT
+from jina import Client, Document
 
 search_terms = ("Dress", "Shirt", "Shoe")
 
+# Server
 
 def plot_search_results(resp: Request):
     for doc in resp.docs:
@@ -15,7 +16,7 @@ def plot_search_results(resp: Request):
         print('-'*10)
         show_docs(doc.matches[:3])
 
-def input_docs(data_path):
+def input_docs(data_path, max_docs):
     for fn in glob.glob(os.path.join(data_path, '*'))[:MAX_DOCS]:
         doc = Document(uri=fn, tags={'filename': fn})
         doc.load_uri_to_image_blob()
@@ -26,3 +27,12 @@ def show_docs(docs):
       plt.imshow(doc.blob)
       plt.show()
 
+
+# Client
+
+def get_matches(input, server=SERVER, port=PORT, max_docs=20):
+    client = Client(host=SERVER, protocol="http", port=PORT)
+    response = client.search(Document(text="foo"), return_results=True)
+    matches = response[0].docs[0].matches
+
+    return matches
