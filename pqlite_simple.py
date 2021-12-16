650d0565dc36a09236dea6bf3ffd35ea0ae2f265
Alex C-G <alexcg@outlook.com>
2021-12-16 15:01:24 +0100

    fix: protocol, port

--- pqlite_simple.py
+++ pqlite_simple.py
@@ -1,5 +1,10 @@
-from jina import Flow
+from jina import Flow, Executor, requests
 from helper import input_docs_from_csv
+
+class GetDocCount(Executor):
+    @requests
+    def get_count(self, docs, **kwargs):
+        print(f"\n\nThere are {len(docs)} in the DocumentArray\n\n")
 
 
 docs = input_docs_from_csv("data/styles.csv", 10)
@@ -12,6 +17,7 @@ flow = (
         name="image_encoder",
         uses_with={"device": "cpu"},
     )
+    # .add(uses=GetDocCount)
     .add(
         uses="jinahub://PQLiteIndexer/latest",
         uses_with={
@@ -31,4 +37,6 @@ flow = (
 
 with flow:
     flow.index(inputs=docs, show_progress=True)
+    flow.port_expose=12345
+    flow.protocol="http"
     flow.block()
