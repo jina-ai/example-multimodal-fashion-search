2546f52e743d77aedda653acf8bb2a4e686aa30a
Alex C-G <alexcg@outlook.com>
2021-12-16 14:43:02 +0100

    chore: cleanup

--- pqlite_query.py
+++ pqlite_query.py
@@ -1,8 +1,8 @@
 from jina import Document, Client
-from config import SERVER, PORT, MAX_DOCS
+from config import SERVER, PORT, TOP_K
 
 
-def get_matches(input, server=SERVER, port=PORT, limit=MAX_DOCS):
+def get_matches(input, server=SERVER, port=PORT, limit=TOP_K):
     client = Client(host=server, protocol="http", port=port)
     response = client.search(Document(text=input), return_results=True, parameters={"limit": limit}, show_progress=True)
     matches = response[0].docs[0].matches
--- pqlite_simple.py
+++ pqlite_simple.py
@@ -1,24 +1,5 @@
-from jina import Flow, Executor, requests, Document, Client
+from jina import Flow
 from helper import input_docs_from_csv
-
-
-class DimGetter(Executor):
-    @requests
-    def get_dims(self, docs, **kwargs):
-        print(docs[0].embedding.shape)
-
-
-def get_matches(input, server=SERVER, port=PORT, limit=MAX_DOCS):
-    client = Client(host=server, protocol="http", port=port)
-    response = client.search(
-        Document(text=input),
-        return_results=True,
-        parameters={"limit": limit},
-        show_progress=True,
-    )
-    matches = response[0].docs[0].matches
-
-    return matches
 
 
 docs = input_docs_from_csv("data/styles.csv", 10)
@@ -30,11 +11,6 @@ flow = (
         uses="jinahub+docker://CLIPImageEncoder",
         name="image_encoder",
         uses_with={"device": "cpu"},
-    )
-    .add(
-        uses=DimGetter,
-        # name="image_encoder",
-        # uses_with={"device": "cpu"},
     )
     .add(
         uses="jinahub://PQLiteIndexer/latest",
@@ -53,8 +29,6 @@ flow = (
     )
 )
 
-query = Document(text="blue dress")
-
 with flow:
     flow.index(inputs=docs, show_progress=True)
-    results = flow.search(inputs=query, return_results=True)
+    flow.block()
