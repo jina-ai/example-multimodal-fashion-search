f3ec9fd0ae759e828046270c244ae9aced79e47a
Alex C-G <alexcg@outlook.com>
2022-01-17 16:36:06 +0000

    fix: server urls

--- frontend/config.py
+++ frontend/config.py
@@ -9,9 +9,9 @@ IMAGE_RESIZE_FACTOR = 3
 DEBUG = True
 
 # serving via REST - text to image
-TEXT_IMAGE_SERVER = os.getenv("BACKEND_SERVER", "0.0.0.0")
+TEXT_IMAGE_SERVER = os.getenv("TEXT_BACKEND_SERVER", "0.0.0.0")
 TEXT_IMAGE_PORT = 12345
 
 # serving via REST - image to image
-IMAGE_IMAGE_SERVER = os.getenv("BACKEND_SERVER", "0.0.0.0")
+IMAGE_IMAGE_SERVER = os.getenv("IMAGE_BACKEND_SERVER", "0.0.0.0")
 IMAGE_IMAGE_PORT = 60000
