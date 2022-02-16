3274f2f309cfdb861dfb8c47297c90ace31b2242
Alex C-G <alexcg@outlook.com>
2022-02-16 18:02:44 +0100

    debug: grpc/rest search interfaces only return 1 result and equal to query

--- backend-text/app.py
+++ backend-text/app.py
@@ -1,9 +1,9 @@
+from docarray import Document
 from jina import Flow
 from helper import input_docs_from_csv, get_columns
 from config import DEVICE, MAX_DOCS, WORKSPACE_DIR, PORT, CSV_FILE, DIMS, TIMEOUT_READY
 import click
 import pickle
-import sys
 
 def index(csv_file=CSV_FILE, max_docs=MAX_DOCS):
     docs = input_docs_from_csv(file_path=csv_file, max_docs=max_docs)
@@ -48,6 +48,37 @@ def index(csv_file=CSV_FILE, max_docs=MAX_DOCS):
 def search():
     columns = pickle.load(open("columns.p", "rb"))
     flow_search = (
+        Flow(protocol="http", port_expose=PORT)
+        .add(
+            uses="jinahub://CLIPTextEncoder/v0.2",
+            name="text_encoder",
+            uses_with={"device": DEVICE},
+            install_requirements=True,
+        )
+        .add(
+            uses="jinahub://PQLiteIndexer/v0.2.3-rc",
+            name="indexer",
+            uses_with={
+                'dim': DIMS,
+                'columns': columns,
+                'metric': "cosine",
+                'include_metadata': True
+            },
+            uses_metas={"workspace": WORKSPACE_DIR},
+            volumes=f"./{WORKSPACE_DIR}:/workspace/workspace",
+            install_requirements=True
+        )
+    )
+
+    with flow_search:
+        # flow_search.port_expose = PORT
+        # flow_search.cors = True
+        # flow_search.protocol = "http"
+        flow_search.block()
+
+def search_grpc():
+    columns = pickle.load(open("columns.p", "rb"))
+    flow_search = (
         Flow()
         .add(
             uses="jinahub://CLIPTextEncoder/v0.2",
@@ -71,17 +102,15 @@ def search():
     )
 
     with flow_search:
-        flow_search.port_expose = PORT
-        flow_search.cors = True
-        flow_search.protocol = "http"
-        flow_search.block()
+        results = flow_search.search(Document(text="shoes"), return_results=True)
+        print([result.text for result in results])
 
 
 @click.command()
 @click.option(
     "--task",
     "-t",
-    type=click.Choice(["index", "search"], case_sensitive=False),
+    type=click.Choice(["index", "search", "search_grpc"], case_sensitive=False),
 )
 @click.option("--num_docs", "-n", default=MAX_DOCS)
 def main(task: str, num_docs: int):
@@ -89,6 +118,8 @@ def main(task: str, num_docs: int):
         index(csv_file=CSV_FILE, max_docs=num_docs)
     elif task == "search":
         search()
+    elif task == "search_grpc":
+        search_grpc()
 
 
 if __name__ == "__main__":
