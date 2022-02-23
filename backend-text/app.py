8a2cc74008f4f91d7fe30d1e1b523810cadc487b
Alex C-G <alexcg@outlook.com>
2022-02-23 12:10:05 +0100

    feat(backend-text): use gpu hub executors if device is cuda

--- backend-text/app.py
+++ backend-text/app.py
@@ -1,20 +1,67 @@
-from docarray import Document
+from docarray import Document, DocumentArray
 from jina import Flow
 from helper import input_docs_from_csv, get_columns
 from config import DEVICE, MAX_DOCS, WORKSPACE_DIR, PORT, CSV_FILE, DIMS, TIMEOUT_READY
 import click
 import pickle
 
+if DEVICE == "cuda":
+    gpu_bool = "-gpu"
+else:
+    gpu_bool = ""
+
+def index_from_pulled_docarray():
+    # docs = input_docs_from_csv(file_path=csv_file, max_docs=max_docs)
+    docs = DocumentArray.pull(f'fashion-multimodal-{MAX_DOCS}')
+
+    columns = get_columns(docs[0])  # Get all the column info from first doc
+    pickle.dump(
+        columns, open("../columns.p", "wb")
+    )  # Pickle values so search fn can pick up later
+
+    flow = (
+        Flow()
+        # .add(
+            # uses=f"jinahub://CLIPImageEncoder/v0.4{gpu_bool}",
+            # name="image_encoder",
+            # uses_with={"device": DEVICE},
+            # install_requirements=True,
+            # uses_metas={"timeout_ready": TIMEOUT_READY},
+        # )
+        .add(
+            uses="jinahub://PQLiteIndexer/latest",
+            name="indexer",
+            uses_with={
+                "dim": DIMS,
+                "columns": columns,
+                "metric": "cosine",
+                "include_metadata": True,
+            },
+            uses_metas={"workspace": WORKSPACE_DIR},
+            volumes=f"./{WORKSPACE_DIR}:/workspace/workspace",
+            install_requirements=True,
+        )
+    )
+
+    with flow:
+        docs = flow.index(inputs=docs, show_progress=True, return_results=True)
+
+    docs.push(f'fashion-multimodal-{MAX_DOCS}')
+
+
+
 def index(csv_file=CSV_FILE, max_docs=MAX_DOCS):
     docs = input_docs_from_csv(file_path=csv_file, max_docs=max_docs)
 
-    columns = get_columns(docs[0]) # Get all the column info from first doc
-    pickle.dump(columns, open("../columns.p", "wb")) # Pickle values so search fn can pick up later
+    columns = get_columns(docs[0])  # Get all the column info from first doc
+    pickle.dump(
+        columns, open("../columns.p", "wb")
+    )  # Pickle values so search fn can pick up later
 
     flow = (
         Flow()
         .add(
-            uses="jinahub://CLIPImageEncoder/v0.4",
+            uses=f"jinahub://CLIPImageEncoder/v0.4{gpu_bool}",
             name="image_encoder",
             uses_with={"device": DEVICE},
             install_requirements=True,
@@ -24,19 +71,22 @@ def index(csv_file=CSV_FILE, max_docs=MAX_DOCS):
             uses="jinahub://PQLiteIndexer/latest",
             name="indexer",
             uses_with={
-                'dim': DIMS,
-                'columns': columns,
-                'metric': "cosine",
-                'include_metadata': True
+                "dim": DIMS,
+                "columns": columns,
+                "metric": "cosine",
+                "include_metadata": True,
             },
             uses_metas={"workspace": WORKSPACE_DIR},
             volumes=f"./{WORKSPACE_DIR}:/workspace/workspace",
-            install_requirements=True
+            install_requirements=True,
         )
     )
 
     with flow:
-        flow.index(inputs=docs, show_progress=True)
+        docs = flow.index(inputs=docs, show_progress=True, return_results=True)
+
+    print(docs)
+    docs.push(f'fashion-multimodal-{MAX_DOCS}')
 
 
 def search():
@@ -44,7 +94,7 @@ def search():
     flow = (
         Flow(protocol="http", port_expose=PORT)
         .add(
-            uses="jinahub://CLIPTextEncoder/v0.2",
+            uses="jinahub://CLIPTextEncoder/v0.2{gpu_bool}",
             name="text_encoder",
             uses_with={"device": DEVICE},
             install_requirements=True,
@@ -53,19 +103,20 @@ def search():
             uses="jinahub://PQLiteIndexer/latest",
             name="indexer",
             uses_with={
-                'dim': DIMS,
-                'columns': columns,
-                'metric': "cosine",
-                'include_metadata': True
+                "dim": DIMS,
+                "columns": columns,
+                "metric": "cosine",
+                "include_metadata": True,
             },
             uses_metas={"workspace": WORKSPACE_DIR},
             volumes=f"./{WORKSPACE_DIR}:/workspace/workspace",
-            install_requirements=True
+            install_requirements=True,
         )
     )
 
     with flow:
         flow.block()
+
 
 def search_grpc():
     columns = pickle.load(open("../columns.p", "rb"))
@@ -81,14 +132,14 @@ def search_grpc():
             uses="jinahub://PQLiteIndexer/latest",
             name="indexer",
             uses_with={
-                'dim': DIMS,
-                'columns': columns,
-                'metric': "cosine",
-                'include_metadata': True
+                "dim": DIMS,
+                "columns": columns,
+                "metric": "cosine",
+                "include_metadata": True,
             },
             uses_metas={"workspace": WORKSPACE_DIR},
             volumes=f"./{WORKSPACE_DIR}:/workspace/workspace",
-            install_requirements=True
+            install_requirements=True,
         )
     )
 
