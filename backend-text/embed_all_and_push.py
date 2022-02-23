8f27bf5bca5f5f87923d1cc6e1ed00710ebd3b01
Alex C-G <alexcg@outlook.com>
2022-02-23 13:25:10 +0100

    feat(embed_push): script so index only once

---
+++ backend-text/embed_all_and_push.py
@@ -0,0 +1,51 @@
+from docarray import Document, DocumentArray
+from jina import Flow
+from helper import input_docs_from_csv, get_columns
+from config import DEVICE, WORKSPACE_DIR, PORT, CSV_FILE, DIMS, TIMEOUT_READY
+import click
+import pickle
+
+MAX_DOCS = 10
+
+pushed_name = "fashion-product-images-clip-all"
+
+def embed_docs(csv_file=CSV_FILE, max_docs=MAX_DOCS):
+    docs = input_docs_from_csv(file_path=csv_file, max_docs=max_docs)
+
+    columns = get_columns(docs[0])  # Get all the column info from first doc
+    pickle.dump(
+        columns, open("../columns.p", "wb")
+    )  # Pickle values so search fn can pick up later
+
+    flow = (
+        Flow()
+        .add(
+            uses=f"jinahub://CLIPImageEncoder/v0.4",
+            name="image_encoder",
+            uses_with={"device": DEVICE},
+            install_requirements=True,
+            uses_metas={"timeout_ready": TIMEOUT_READY},
+            replicas=2,
+        )
+        # .add(
+            # uses="jinahub://PQLiteIndexer/latest",
+            # name="indexer",
+            # uses_with={
+                # "dim": DIMS,
+                # "columns": columns,
+                # "metric": "cosine",
+                # "include_metadata": True,
+            # },
+            # uses_metas={"workspace": WORKSPACE_DIR},
+            # volumes=f"./{WORKSPACE_DIR}:/workspace/workspace",
+            # install_requirements=True,
+        # )
+    )
+
+    with flow:
+        docs = flow.index(inputs=docs, show_progress=True, return_results=True)
+
+    return docs
+
+embedded_docs = embed_docs()
+embedded_docs.push(pushed_name)
