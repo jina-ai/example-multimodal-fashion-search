264afbe5cf07d4ddc9c9fbfd03d3136db0c09e4d
Alex C-G <alexcg@outlook.com>
2022-03-04 17:16:56 +0100

    fix(indexer): correct max_docs with cli arg for real

--- indexer/app.py
+++ indexer/app.py
@@ -10,11 +10,9 @@ else:
     gpu_bool = ""
 
 if len(sys.argv) == 2:
-    MAX_DOCS = sys.argv[1]
+    MAX_DOCS = int(sys.argv[1])
 
 print(f"Indexing {MAX_DOCS} documents")
-import sys
-sys.exit()
 
 def index(csv_file, max_docs):
     docs = csv_to_docarray(file_path=csv_file, max_docs=max_docs)
